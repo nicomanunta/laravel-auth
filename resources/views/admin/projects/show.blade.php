a749c53dec153e37dea037e8b2e6706f0c22bffe
nicomanunta <nic.manun@gmail.com>
2024-02-21 20:10:48 +0100

    esercizio finito

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,10 +7,11 @@
             <div class="col-12">
                 <img src="{{$project->cover_immagine}}" alt="">
                 <h2>{{$project->nome_progetto}}</h2>
-                <p>{{$project->descrizione}}</p>
                 <span>{{$project->slug}}</span>
-                <br>
-                <span>{{$project->data}}</span>
+                <p class="mt-5">{{$project->descrizione}}</p>
+                
+                <span class="mt-5">data: {{$project->data}}</span>
+                
             </div>
         </div>
     </div>
