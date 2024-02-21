2a75516e754c8b1f9591bea3aa201ee420aaed64
nicomanunta <nic.manun@gmail.com>
2024-02-21 14:47:35 +0100

    lanciato comando per model e rcmsR

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function(){
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-})
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
